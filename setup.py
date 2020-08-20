5843bd681a591c349c9b38aae075420518e466e1
Steen Manniche <steen@manniche.net>
2020-08-20 18:36:26 +0200

    Add support for GitLab flavoured markdown
    suitable for the gitlab wiki

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
     setuptools.setup(
-        name='markdown-tree',  
-        version='0.1.5',
+        name='markdown-tree',
+        version='0.2.0',
         scripts=['markdown_tree'] ,
-        author="Jiaming Lu",
-        author_email="jiaminglu@live.com",
+        author="Jiaming Lu, Steen Manniche",
+        author_email="jiaminglu@live.com, boxunbox@gmail.com",
         description="Generate file tree in markdown format",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/jiaminglu/markdown-tree",
+        url="https://github.com/manniche/markdown-tree",
         classifiers=[
           'Environment :: Console',
           'License :: OSI Approved :: MIT License',
